4d5677950fd2d03405c6aaf67be6029186ef97c1
henriqueaguiiar <henriqueaguiarpacheco09@gmail.com>
2025-06-26 19:24:55 -0300

    feature: implements LIbrary

--- src/br/com/henriqueaguiiar/entities/Library.java
+++ src/br/com/henriqueaguiiar/entities/Library.java
@@ -1,7 +1,33 @@
+import br.com.henriqueaguiiar.entities.Autor;
+import br.com.henriqueaguiiar.entities.Book;
+import br.com.henriqueaguiiar.entities.Loan;
+import br.com.henriqueaguiiar.utils.ServiceLibrary;
 
-package br.com.henriqueaguiiar.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library implements ServiceLibrary {
+    private List<Book> bookList = new ArrayList<>();
+    private List<Autor> autorsList = new ArrayList<>();
+    private List<Loan> loanList = new ArrayList<>();
 
 
-public class Library {
+    public List<Book> getBookList(){
+        return bookList;
+    }
+
+    public List<Autor> getAutorsList(){
+
+        return autorsList;
+    }
+
+    public List<Loan> getLoanList(){
+        return loanList;
+    }
+
+    public void addToList(Book book){
+        bookList.add(book);
+    }
 
 }
