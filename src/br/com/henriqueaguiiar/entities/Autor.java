a5a2127b9b162a28dc6a05357cdb96ca22756bd8
henriqueaguiiar <henriqueaguiarpacheco09@gmail.com>
2025-06-26 19:20:30 -0300

    feature: implements Autor

--- src/br/com/henriqueaguiiar/entities/Autor.java
+++ src/br/com/henriqueaguiiar/entities/Autor.java
@@ -3,8 +3,42 @@ package br.com.henriqueaguiiar.entities;
 import java.util.Date;
 import java.util.UUID;
 
-public class Autor{
+public class Autor {
+    private String id;
+    private String name;
+    private Date bornDate;
 
 
+    public Autor(String name, Date bornDate) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.bornDate = bornDate;
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBornDate() {
+        return bornDate;
+    }
+
+    public void setBornDate(Date bornDate) {
+        this.bornDate = bornDate;
+    }
+
+    @Override
+    public String toString() {
+        return "\nId: " + this.id
+                + "\nAutor Name: " + this.name
+                + "\nBorn Date: " + this.bornDate;
+    }
 }
